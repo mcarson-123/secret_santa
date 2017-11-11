c3cec5d89e4897e993f5dbed64d8c64bb9291479
Madeline Carson <madeline.carson@gmail.com>
2017-11-11 09:10:20 -0800

    Use sendgrid for emails - Update environment to use sendgrid as the default mailer for ActionMailer - Was attempting gmail previously, but gmail 'trust machine' permission errors with sending from Heroku was causing issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Email
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => ENV['GMAIL_USERNAME'],
+  #   :password             => ENV['GMAIL_PASSWORD'],
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
 end
